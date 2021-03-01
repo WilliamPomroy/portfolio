67b2d28a1819094a718e58fcda7c1fcc4815f13b
WilliamPomroy <williampomroy03@gmail.com>
2021-03-01 16:30:37 -0700

    Nav Bar Setup
    Made the navbar.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// This is some js
+window.addEventListener("DOMContentLoaded", function(event) {
+    const menu = document.querySelector('.site-nav');
+    const menuButton = document.querySelector(".menu-icon");
+    
+    menuButton.onclick = (e) => {
+        menu.classList.toggle("right");
+    }
+
+    menu.onmouseleave = (e) => {
+        menu.classList.toggle('right');
+    }
+});
