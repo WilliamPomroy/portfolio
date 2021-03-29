613eecfb769045e2d8830714a201bd8603eea05f
WilliamPomroy <williampomroy03@gmail.com>
2021-03-29 12:31:46 -0600

    Updating Layout of Header and Colors
    Updated the sliders content and positioned it, got some basic colors down just to get an idea of what is going to be happening on the site.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ const clickButton = document.querySelector('.toggle-icon');
 // Step 2: add a click event to that icon
 clickButton.addEventListener('click', () => {
     // when that icon is clicked we are going to grab the nav element (or whatever is showing/hiding) and add or remove that special class we created in the CSS
-    document.querySelector('nav').classList.toggle('show-nav');
+    document.querySelector('nav').classList.toggle('left');
     clickButton.classList.toggle('open');
 });
