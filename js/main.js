e2ab725abfa4fd8b8896660f2efa5127cd112542
WilliamPomroy <williampomroy03@gmail.com>
2021-09-04 04:15:07 -0600

    Finished for the Day
    Finished the index page.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-// Step 1: select the element the user will click on to make this menu show/hide. In this case it's the toggle-icon and since we are grabbing it by it's classname we need to include the period 
-const clickButton = document.querySelector('.toggle-icon');
+// // Step 1: select the element the user will click on to make this menu show/hide. In this case it's the toggle-icon and since we are grabbing it by it's classname we need to include the period 
+// const clickButton = document.querySelector('.toggle-icon');
 
 
-// Step 2: add a click event to that icon
-clickButton.addEventListener('click', () => {
-    // when that icon is clicked we are going to grab the nav element (or whatever is showing/hiding) and add or remove that special class we created in the CSS
-    document.querySelector('nav').classList.toggle('left');
-    clickButton.classList.toggle('open');
+// // Step 2: add a click event to that icon
+// clickButton.addEventListener('click', () => {
+//     // when that icon is clicked we are going to grab the nav element (or whatever is showing/hiding) and add or remove that special class we created in the CSS
+//     document.querySelector('nav').classList.toggle('left');
+//     clickButton.classList.toggle('open');
+// });
+
+// 
+
+const portBtn = document.querySelector('.portfolio-btn');
+const folder = document.querySelector('.class-5');
+const shadow = document.querySelector('.class-6');
+
+portBtn.addEventListener('mouseover', () => {
+    folder.classList.toggle('folder-open');
+    shadow.classList.toggle('folder-open');
+});
+
+portBtn.addEventListener('mouseleave', () => {
+    folder.classList.toggle('folder-open');
+    shadow.classList.toggle('folder-open');
 });
